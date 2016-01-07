5d55e4d268f7ab22fa8546ef0af7134b9508c0a0
hammad khan <hammad.rehan@live.com>
2016-01-07 16:26:03 +0500

    First commit

---
+++ Mini_E_Pay/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Mini_E_Pay.MvcApplication" Language="C#" %>
---
+++ Mini_E_Pay/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Mini_E_Pay.Startup))]
+namespace Mini_E_Pay
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
